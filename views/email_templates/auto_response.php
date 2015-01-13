cd5d6a66a65384d2217d30a01d664dfabcb916c3
Gordon Murray <murrion@users.noreply.github.com>
2015-01-13 09:41:01 +0000

    Adding a View, used by an auto response email

---
+++ views/email_templates/auto_response.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Email Response</title>
+</head>
+<body>
+
+Hi,
+
+<p>Thank you for your email. A member of the Team will contact you shortly.</p>
+
+<p>Thank you,</p>
+
+</body>
+</html>
